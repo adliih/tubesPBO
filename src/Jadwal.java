62a7ced591655cd32c500f8d0bd4672fa9a74b29
Andrids <dinandri11@gmail.com>
2015-04-29 19:33:54 +0700

    Revert "Gui"
    This reverts commit 8388c6393d227544f406aacd7ab780361e818bbd.

--- src/Jadwal.java
+++ src/Jadwal.java
@@ -6,40 +6,21 @@ public class Jadwal {
     protected String logBook = "LOGBOOK";
 
     public void addPenyewa(Member member) {
-        if (penyewa != null){
-            throw new IllegalArgumentException("Jadwal Sudah Dipesan");
-        }
         penyewa = member;
         logBook += "\nmember " + penyewa.getUsername() + " telah membooking lapangan";
     }
 
     public Member getPenyewa() {
-        if (penyewa == null){
-            throw new IllegalArgumentException("Jadwal Masih Belum Disewa"); 
-        }
         return penyewa;
     }
 
     public String getLogBook() {
         return logBook;
     }
-    
-    /*
-    Menampilkan Status Dari Lapangan (Tersedia/disewa/sudah dibooking)
-    */
+
     @Override
     public String toString() {
-        String x =  "";
-        if (penyewa == null)
-            x += "Tersedia";
-        else{
-            x += "Sudah dibooking a/n " + penyewa.getNama();
-            if (statusDP){
-                x+= " [Sudah DP]";
-            }
-        }
-        return x;
-        
+        return " ";
     }
 
     public void DP() {
@@ -47,9 +28,7 @@ public class Jadwal {
         logBook += "\nmember " + penyewa.getUsername() + " telah membayar lapangan";
     }
 
-    public void batal() {
+    public void Batal() {
         logBook += "\nmember " + penyewa.getUsername() + " telah membatal menyewa lapangan";
-        statusDP = false;
-        penyewa = null;
     }
 }
--- src/TempatFutsal.java
+++ src/TempatFutsal.java
@@ -26,131 +26,56 @@ public class TempatFutsal {
 
     public String getNama() {
         return nama;
+
     }
 
     public String getAlamat() {
         return alamat;
     }
 
-    public int getNLap() {
-        return nLap;
-    }
-//    Membuat Lapangan Baru
-//    1 = Lapangan Sintetis
-//    2 = Lapangan Vinyl
-
     public void createLapangan(int x, double lebar, double panjang) {
-        //Mencoba menambah array lapangan
-        if (nLap < 5) {
-            if (x == 0) {
-                lapangan[nLap++] = new Sintetis(lebar, panjang);
-            } else if (x == 1) {
-                lapangan[nLap++] = new Vinyl(lebar, panjang);
-            }
-        } else {
-            //Melempar Kesalahan Ketika nLap sudah maksimal
-            throw new RuntimeException("Array Lapangan Sudah Penuh");
+        if (x == 0) {
+            lapangan[nLap++] = new Sintetis(lebar, panjang);
+        } else if (x == 1) {
+            lapangan[nLap++] = new Vinyl(lebar, panjang);
         }
-    }
-
-    /*
-     Delete Lapangan akan mengganti isi lapangan pada id yang ditentukan dengan indeks selanjutnya.
-     Proses akan berlanjut hingga akhir array lapangan
-     Lapangan terakhir akan diisi dengan null dan jumlah lapangan akan dkurangi
-     */
-    public void deleteLapangan(int id) {
-        if (id >= nLap || nLap < 0) {
-            //Melempar Kesalahan akses indeks Array
-            throw new ArrayIndexOutOfBoundsException("Kesalahan Akses Indeks Array Lapangan");
-        }
-        for (int j = id; j < (nLap - 1); j++) {
-            lapangan[j] = lapangan[j + 1];
-        }
-        lapangan[--nLap] = null;
     }
 
     public Lapangan getLapangan(int id) {
-        Lapangan lap = null;
-        try {
-            lap = lapangan[id];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //Melempar Kesalahan akses indeks Array
-            throw new ArrayIndexOutOfBoundsException("Kesalahan Akses Indeks Array Lapangan");
-        }
-        return lap;
-    }
-
-    public void viewLapangan() {
-        try {
-            for (int i = 0; i < nLap; i++) {
-                System.out.println("Lapangan " + i + ". " + lapangan[i].getClass().toString().substring(6) + " " + lapangan[i].getLebar() + " x " + lapangan[i].getPanjang() + " berharga " + lapangan[i].getHarga() + " per jam");
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //Melempar Kesalahan akses indeks Array
-            throw new ArrayIndexOutOfBoundsException("Kesalahan Akses Indeks Array Lapangan");
-        }
+        return lapangan[id];
     }
 
     public Member getMember(int id) {
-        Member m = null;
-        try {
-            m = member[id];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //Melempar Kesalahan akses indeks Array
-            throw new ArrayIndexOutOfBoundsException("Kesalahan Akses Indeks Array Member");
-        }
-        return m;
+        return member[id];
     }
 
     public void addMember(Member member) {
-        if (nMember < 100) {
-            this.member[nMember++] = member;
-        } else {
-            //Melempar Kesalahan Ketika nMember sudah maksimal
-            throw new RuntimeException("Array Member Sudah Penuh");
-        }
+        this.member[nMember++] = member;
     }
 
-    /*
-     Fungsi mencari member berdasarkan username
-     Mengembalikan indeks member jika ditemukan
-     Mengembalikan -1 jika member tidak ditemukan
-     */
     public int searchMember(String username) {
-        int tmp = -1;
+        int tmp = 0;
         for (int i = 0; i < nMember; i++) {
-            if (member[i].getUsername().equals(username)) {
+            if (member[i].getUsername() == username) {
                 tmp = i;
-                break;  //Menghentikan loop agar tidak terlalu lama
             }
         }
         return tmp;
     }
-    /*
-     Menghapus member berdasarkan id yang dimasukkan
-     */
 
     public void deleteMember(int id) {
-        try {
-            for (int j = id; j < nMember - 1; j++) {
-                member[j] = member[j + 1];
-            }
-            member[nMember--] = null;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //Melempar Kesalahan akses indeks Array
-            throw new ArrayIndexOutOfBoundsException("Kesalahan Akses Indeks Array Member");
+
+        for (int j = id; j < nMember; j++) {
+            member[j] = member[j + 1];
         }
+        nMember--;
+
     }
 
     public void viewMember() {
-        try {
-            for (int i = 0; i < nMember; i++) {
-                System.out.println((i + 1) + ". " + member[i].getUsername());
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //Melempar Kesalahan akses indeks Array
-            throw new ArrayIndexOutOfBoundsException("Kesalahan Akses Indeks Array Member");
-        }
 
+        for (int i = 0; i < nMember; i++) {
+            System.out.println((i + 1) + ". " + member[i].getUsername());
+        }
     }
 }
